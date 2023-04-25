85c0574d018e52741a839e832b3fabaadd2ae149
Marlene <rouzier@serpe.fr>
2023-04-25 12:56:15 +0200

    chart, recharts, highcharts

--- chartjs/graphe.js
+++ chartjs/graphe.js
@@ -1,17 +1,19 @@
 const ctx = document.getElementById('myChart');
 
 new Chart(ctx, {
-  type: 'bar',
+  type: 'bar', // ou pie, doughnut, line, polarArea, bar, radar
   data: {
-    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange', 'white'],
     datasets: [{
       label: '# of Votes',
-      data: [12, 19, 3, 5, 2, 3],
+      data: [12, 19, 3, 5, 2, 3, 16],
       borderWidth: 1,
-      backgroundColor: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+      backgroundColor: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange', 'white'],
       borderColor: ['teal'],
       borderWidth: 2,
-      // hoverBackgroundColor: ['Pink', 'Teal', 'Yellowgreen', 'Greenyellow', 'rebeccapurple', 'Orangered']
+      // hoverBackgroundColor: ['Pink', 'Teal', 'Yellowgreen', 'Greenyellow', 'rebeccapurple', 'Orangered', 'grey']
+      // hoverBorderWidth: 0,
+      borderRadius: 10
     }]
   },
   options: {
